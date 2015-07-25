0ce21c74ac2e121547a742f5fd2bf15231c1ad65
Jake Russo <madcap.russo@gmail.com>
2015-07-25 17:10:50 -0500

    Add marked and call it

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const marked = require("marked")
 module.exports = function () {
   this.filter("transformerName", (source, options) => {
     try {
-      return source
+      return marked(src)
      } catch (e) { throw e }
   })
 }
