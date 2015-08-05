7f4bfcfc494d492f03f3a3657c1f2ecec2f35689
Jake Russo <madcap.russo@gmail.com>
2015-08-05 10:57:06 -0500

    Add tests :boom:

--- test/test.js
+++ test/test.js
@@ -1,5 +1,14 @@
 const test = require("tape").test
+const marked = require("../")
 
-test("fly-marked", function (t) {
-  t.end()
+test("fly-marked", (t) => {
+  t.plan(3)
+  marked.call({
+    filter: function (name, transform, options) {
+      t.equal(name, "marked", "add marked filter")
+      t.ok(/<p>I am using <strong>markdown<\/strong>\.<\/p>/.test(
+        transform("I am using __markdown__.")), "marked transform")
+      t.equal(options.ext, ".md", "extension is .md")
+    }
+  })
 })
